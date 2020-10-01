c3bd330593d42c71ff282d2c9079395916a2947b
Mohamed ElSaadany <21214427+sa3dany@users.noreply.github.com>
2020-10-01 18:06:33 +0300

    add xolor extension snippet to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,16 +9,19 @@ module.exports = {
     purgeLayersByDefault: true,
     removeDeprecatedGapUtilities: true,
   },
-  plugins: [require('@tailwindcss/typography'), require('@tailwindcss/ui')],
-  purge: ['cms/templates/**/*.twig', 'src/js/**/*.js'],
+  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/ui")],
+  purge: ["cms/templates/**/*.twig", "src/js/**/*.js"],
   theme: {
     container: {
       center: true,
       padding: defaultTheme.spacing.s4,
     },
     extend: {
+      colors: {
+        "brand-black": "#000",
+      },
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
       },
     },
   },
