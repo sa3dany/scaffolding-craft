3a529121653dfbd1976a3a521ebb80e82dda526a
Mohamed ElSaadany <21214427+sa3dany@users.noreply.github.com>
2020-10-02 20:48:29 +0300

    apply prettier formatting

--- postcss.config.js
+++ postcss.config.js
@@ -1,20 +1,20 @@
 const MANIFEST_PATH = "cms/web/assets/site/css/manifest.json";
 
-const production = process.env.NODE_ENV == 'production';
+const production = process.env.NODE_ENV == "production";
 
 const plugins = [];
 
 plugins.push(
-  require('postcss-import'),
-  require('tailwindcss'),
-  require('postcss-rtl'),
-  require('autoprefixer')
+  require("postcss-import"),
+  require("tailwindcss"),
+  require("postcss-rtl"),
+  require("autoprefixer")
 );
 
 production &&
   plugins.push(
-    require('cssnano')({ preset: 'default' }),
-    require('postcss-hash')({ manifest: MANIFEST_PATH })
+    require("cssnano")({ preset: "default" }),
+    require("postcss-hash")({ manifest: MANIFEST_PATH })
   );
 
 module.exports = { plugins };
